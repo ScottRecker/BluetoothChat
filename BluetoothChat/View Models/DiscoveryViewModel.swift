cfa2b903bc460805f7be3509eb3846f3a70ab577
Scott Recker <scott.a.recker@gmail.com>
2025-05-04 12:10:10 -0500

    Add ability to scan for bluetooth devices

--- BluetoothChat/View Models/DiscoveryViewModel.swift
+++ BluetoothChat/View Models/DiscoveryViewModel.swift
@@ -5,10 +5,80 @@
 //  Created by Scott Recker on 5/4/25.
 //
 
+import CoreBluetooth
 import SwiftUI
 
 @Observable
 class DiscoveryViewModel: NSObject {
+    var isBluetoothEnabled: Bool = false
+    var discoverablePeripherals: [CBPeripheral] = []
+
+    private var centralManager: CBCentralManager!
+    private var peripheralArray: [CBPeripheral] = []
+    private var rssiArray = [NSNumber]()
+    private var timer = Timer()
+    private var bluefruitPeripheral: CBPeripheral!
+
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func startScanning() -> Void {
+        // Remove prior data
+        peripheralArray.removeAll()
+        rssiArray.removeAll()
+        // Start Scanning
+        centralManager?.scanForPeripherals(withServices: [CBUUIDs.BLEService_UUID])
+        Timer.scheduledTimer(withTimeInterval: 15, repeats: false) {_ in
+            self.stopScanning()
+        }
+    }
+
+    func stopScanning() -> Void {
+        centralManager?.stopScan()
+    }
 
 }
 
+extension DiscoveryViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOff:
+            print("Is Powered Off.")
+        case .poweredOn:
+            print("Is Powered On.")
+            startScanning()
+        case .unsupported:
+            print("Is Unsupported.")
+        case .unauthorized:
+            print("Is Unauthorized.")
+        case .unknown:
+            print("Unknown")
+        case .resetting:
+            print("Resetting")
+        @unknown default:
+            print("Error")
+        }
+    }
+
+    func centralManager( _ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber ) {
+
+        bluefruitPeripheral = peripheral
+
+        if peripheralArray.contains(peripheral) {
+            print("Duplicate Found.")
+        } else {
+            peripheralArray.append(peripheral)
+            rssiArray.append(RSSI)
+        }
+
+        print("Peripheral Discovered: \(peripheral)")
+    }
+
+    // MARK: - Connect
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        stopScanning()
+        bluefruitPeripheral.discoverServices([CBUUIDs.BLEService_UUID])
+    }
+}
