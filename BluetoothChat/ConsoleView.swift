349a14cba56b6b8c22f2413a75f78d585a382e03
Scott Recker <scott.a.recker@gmail.com>
2025-05-04 16:16:53 -0500

    Add console view to app

---
+++ BluetoothChat/ConsoleView.swift
@@ -0,0 +1,22 @@
+//
+//  ConsoleView.swift
+//  BluetoothChat
+//
+//  Created by Scott Recker on 5/4/25.
+//
+
+import SwiftUI
+
+
+struct ConsoleView: View {
+    var body: some View {
+        VStack {
+            Text("ConsoleView")
+        }
+    }
+}
+
+
+#Preview {
+    ConsoleView()
+}
---
+++ BluetoothChat/View Models/ConsoleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ConsoleViewModel.swift
+//  BluetoothChat
+//
+//  Created by Scott Recker on 5/4/25.
+//
+
+import SwiftUI
+
+@Observable
+class ConsoleViewModel {
+
+}
